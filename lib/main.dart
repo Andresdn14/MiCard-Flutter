9ddad204181564b05f62b776a3e67073555c812e
Andres Felipe De La Ossa Navarro <andresdelaossa@hotmail.com>
2020-02-11 11:59:55 -0500

    Mi card Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/ossaFix.png'),
+                ),
+                Text(
+                  'Andrés De La Ossa',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'iOS DEVELOPER',
+                  style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      color: Colors.teal.shade100,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5),
+                ),
+                SizedBox(
+                  width: 150,
+                  height: 16,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.add_shopping_cart,
+                        color: Colors.teal.shade900,
+                      ),
+                      title: Text(
+                        '+57 3107396171',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'),
+                      )),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      'andres@adndevlabs.com',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
